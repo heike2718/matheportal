a3947932311bbfafce0dded822b90cea5a833955
Heike Winkelvoß <public@egladil.de>
2026-04-01 19:46:12 +0200

    bff minikaenguru-anwendung

---
+++ apps/minikaenguru-anwendung/src/main/java/de/mathejungalt/minikaenguru/anwendung/infrastructure/cdi/StartupListener.java
@@ -0,0 +1,38 @@
+package de.mathejungalt.minikaenguru.anwendung.infrastructure.cdi;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.configuration.ConfigUtils;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+/**
+ * StartupListener
+ */
+@ApplicationScoped
+public class StartupListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StartupListener.class);
+
+    @ConfigProperty(name = "quarkus.http.cors.origins")
+    String corsOrigins;
+
+    @ConfigProperty(name = "quarkus.http.port")
+    String port;
+
+    /**
+     * On startup.
+     *
+     * @param startupEvent StartupEvent - the startupEvent
+     */
+    void onStartup(@Observes final StartupEvent startupEvent) {
+
+        LOGGER.info(" ===========>  port={}", port);
+        LOGGER.info(" ===========> quarkus.http.cors.origins={}", corsOrigins);
+
+    }
+}
