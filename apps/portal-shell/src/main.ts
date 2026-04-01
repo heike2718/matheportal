ca2a30d657409a418e8f32663704641c6569d516
Heike Winkelvoß <public@egladil.de>
2026-04-01 09:37:28 +0200

    generator-Konfiguration geändert

--- apps/portal-shell/src/main.ts
+++ apps/portal-shell/src/main.ts
@@ -1,6 +1,5 @@
 import { initFederation } from '@angular-architects/native-federation';
 
-// Wir tauschen das Objekt gegen den Pfad zur Datei in 'public' aus
 initFederation('/federation.manifest.json')
     .catch(err => console.error(err))
     .then(_ => import('./bootstrap'))
