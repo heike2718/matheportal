ddce10de98027f036dea2e26ad715ed409df7aa5
Heike Winkelvoß <public@egladil.de>
2026-04-03 11:22:57 +0200

    matheportal-shell served frontend

---
+++ apps/matheportal-shell-ui/src/app/config/configuration.qs.ts
@@ -0,0 +1,8 @@
+import { MatheportalShellConfiguration } from './matheportal-shell.configuration';
+
+export const matheporalShellConfiguration: MatheportalShellConfiguration = {
+    apiUrl: '',
+    environment: 'qs',
+    production: false,
+    version: '0.0.1-SNAPSHOT',
+};
---
+++ apps/matheportal-shell/src/main/java/de/mathejungalt/matheportal/shell/infrastructure/cdi/StartupListener.java
@@ -0,0 +1,35 @@
+package de.mathejungalt.matheportal.shell.infrastructure.cdi;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.event.Observes;
+
+/**
+ * StartupListener.
+ */
+public class StartupListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StartupListener.class);
+
+    @ConfigProperty(name = "quarkus.http.cors.origins")
+    String corsOrigins;
+
+    @ConfigProperty(name = "quarkus.http.port")
+    String port;
+
+    /**
+     * On startup.
+     *
+     * @param startupEvent StartupEvent - the startupEvent
+     */
+    void onStartup(@Observes final StartupEvent startupEvent) {
+
+        LOGGER.info(" ===========>  port={}", port);
+        LOGGER.info(" ===========> quarkus.http.cors.origins={}", corsOrigins);
+
+    }
+
+}
