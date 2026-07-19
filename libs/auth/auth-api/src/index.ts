ee8a33a391c3049ab041a4d702e4231d94df379b
Heike Winkelvoß <public@egladil.de>
2026-07-19 08:09:53 +0200

    Chore: typo in file name for the auth store evets (#32)

--- libs/auth/auth-api/src/index.ts
+++ libs/auth/auth-api/src/index.ts
@@ -3,4 +3,4 @@ export { AuthFlowFacade } from './lib/auth-api/auth-flow.facade';
 export { AuthSessionFacade } from './lib/auth-api/auth-session.facade';
 export { credentialsInterceptor } from './lib/auth-api/credentials.interceptor';
 
-export { userLoggedOut } from './lib/auth-api/authe-store.events';
+export { userLoggedOut } from './lib/auth-api/auth-store.events';
